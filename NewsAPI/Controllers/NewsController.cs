983243914f076013d01886d460e320d855b6a00b
Jeffery Tse <kytseag@ust.hk>
2024-07-31 07:17:22 +0800

    Add project files.

---
+++ NewsAPI/Controllers/NewsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly WebContext _webContext;
+
+        public NewsController(WebContext webContext)
+        {
+            _webContext = webContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<News> Get()
+        {
+            return _webContext.News;
+        }
+
+        // GET api/<NewsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NewsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NewsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NewsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NewsAPI/Models/WebContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewsAPI.Models;
+
+public partial class WebContext : DbContext
+{
+    public WebContext(DbContextOptions<WebContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employee { get; set; }
+
+    public virtual DbSet<News> News { get; set; }
+
+    public virtual DbSet<NewsFiles> NewsFiles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<News>(entity =>
+        {
+            entity.Property(e => e.NewsId).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.EndDateTime).HasColumnType("datetime");
+            entity.Property(e => e.InsertDateTime)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.StartDateTime).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(250);
+            entity.Property(e => e.UpdateDateTime)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<NewsFiles>(entity =>
+        {
+            entity.Property(e => e.NewsFilesId).ValueGeneratedNever();
+            entity.Property(e => e.Extension).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(250);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
